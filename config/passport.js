c43794c4a3d967b4df87f4f70e6f4fbbfee45fb6
Ramchandra Jana <53928966+rj1995-dev@users.noreply.github.com>
2019-11-11 20:30:53 +0530

    config commit

---
+++ config/passport.js
@@ -0,0 +1,56 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("./keys");
+
+//Load User Model
+const User = mongoose.model("users");
+
+module.exports = function(passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback",
+        proxy: true
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // console.log(accessToken);
+        // console.log(profile);
+        // const image = profile.photos[0].value.substring(
+        //   0,
+        //   profile.photos[0].value.indexOf("?")
+        // );
+        // console.log(image);
+        const newUser = {
+          googleID: profile.id,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          email: profile.emails[0].value,
+          image: profile.photos[0].value
+        };
+
+        //check for existing user
+        User.findOne({
+          googleID: profile.id
+        }).then(user => {
+          if (user) {
+            //Return User
+            done(null, user);
+          } else {
+            //Create User
+            new User(newUser).save().then(user => done(null, user));
+          }
+        });
+      }
+    )
+  );
+  //SerializeUser of User
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  //DeSerializeUser of User
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
+};
